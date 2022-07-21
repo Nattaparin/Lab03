2c910cf0f257888425d2753d96a84ffdd7297d4e
Nattaparin <nattaparin455@gmail.com>
2022-07-22 03:49:54 +0700

    4.12

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.export = {
+    singleQuote: true,
+    semi: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: true
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <EventCard1 v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
+<style scoped>
+    .events{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
 
+    }
+</style>
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import EventCard from "@/components/EventCard.vue/";
+import EventCard1 from "@/components/EventCard1.vue/";
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    EventCard,
+    EventCard1
+
   },
+  data(){
+    return{
+      events:[
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          date:'july 22,2022',
+          time:11.00,
+          petsAllowed: false,
+          organizer:'crey wales',
+
+        }
+      ]
+    }
+  }
 };
 </script>
